3c80889b65f911b9330f5d57e6ed8a758b3328ef
jagdish <pkjagdish4501@gmail.com>
2023-10-28 18:47:30 +0530

    time based caching system implemented

--- index.js
+++ index.js
@@ -4,46 +4,54 @@ const PORT = 5000;
 import { index, getQerry } from './Util.js';
 import 'dotenv/config'
 const API_KEY = [process.env.API_KEY1, process.env.API_KEY2, process.env.API_KEY3, process.env.API_KEY4, process.env.API_KEY5]
-console.log(API_KEY)
+
 let hit = 0;
 const memo = {}
+const ExpireTime = 1000 * 60 * 30;
+console.log(ExpireTime)
 
 const server = http.createServer(async (req, res) => {
     let q = getQerry(req.url)
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    console.log(hit, index(hit))
     if (req.url.match(/\/api\/everything/) && req.method === "GET") {
         let url = 'https://newsapi.org/v2/everything?apiKey=' + API_KEY[index(hit)]
         try {
             url = url + '&' + q
             q += 'everything'
-            console.log(url)
             if (!memo[q]) {
                 hit = (hit + 1) % 250
                 const respose = await axios.get(url);
-                memo[q] = respose.data
+                memo[q] = {
+                    data: respose.data,
+                    id: setTimeout(() => { delete memo[q]; console.log('key ', q, " Expired") },
+                        ExpireTime)
+                }
             }
             res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify(memo[q]))
-
+            res.end(JSON.stringify(memo[q].data))
         } catch (error) {
             res.writeHead(404, { "Content-Type": "application/json" });
             res.end(JSON.stringify({ message: error.message, cause: error.name }));
         }
     } else if (req.url.match(/\/api\/top-headlines/) && req.method === "GET") {
-        let url = 'https://newsapi.org/v2/top-headlines?apiKey=' + API_KEY[4]
+        let url = 'https://newsapi.org/v2/top-headlines?apiKey=' + API_KEY[index(hit)]
         try {
             url = url + '&' + q
             q += 'top-headlines'
-            console.log(url)
             if (!memo[q]) {
                 hit = (hit + 1) % 250
                 const respose = await axios.get(url);
-                memo[q] = respose.data
+                memo[q] = {
+                    data: respose.data,
+                    id: setTimeout(() => { delete memo[q]; console.log('key ', q, " Expired") },
+                        ExpireTime)
+                }
             }
             res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify(memo[q]))
+            res.end(JSON.stringify(memo[q].data))
 
         } catch (error) {
             res.writeHead(404, { "Content-Type": "application/json" });
--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 async function fetchData() {
     const url = `http://localhost:5000/api/everything?q=bitcoin`;
+    // const url = `http://localhost:5000/api/everything?q=modi&pageSize=12`;
+    // const url = `http://localhost:5000/api/everything?q=bitcoin&pageSize=5`;
     // const url = `https://newsapi.org/v2/everything?apiKey=30ea0ceca22b42958b4d2fba9a800165`;
     const data = await fetch(url);
     const parsData = await data.json();
-    console.log(parsData)
+    console.log(parsData.articles.length)
 }
 
 fetchData()
