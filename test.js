afaf23001272fdf1a98402daa073d1a5ef66e1bf
jagdish <pkjagdish4501@gmail.com>
2023-10-30 21:37:59 +0530

    rmoving unnecesry file

--- test.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-async function fetchData() {
-    const url = `https://newsapi.org/docs/endpoints`;
-    let data = await axios.get(url);
-    console.log(data.data)
-}
-
-fetchData()
